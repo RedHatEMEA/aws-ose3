85b033160bee439dac91c1e60fc6ed6586cd40d6
Ed Seymour <eseymour@redhat.com>
2015-10-23 10:18:57 +0100

    minor script improvements

--- provision-all.sh
+++ provision-all.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -ex
+#!/bin/bash -x
 
 identity=${1}
 [ "${identity}" = "" ] && echo "** PLEASE PROVIDE A PATH TO AWS IDENTITY CREDENTIALS (PEM FILE)**" && exit 255
@@ -22,9 +22,17 @@ do
 
   echo "*** Updating password for ${name}"
   bin/issh cloud-user@${dns} -i ${identity} -tt "echo ${pwd} | sudo passwd demo --stdin" &>log/log-pwd-$dns < /dev/null
+  
+  [ $? -ne 0 ] && echo "*** ERROR $? RETURNED FOR ${dns} WHILST SETTING DEMO PASSWORD, bailing ***" && exit 1
 
   scp -i ${identity} -S bin/issh target/* cloud-user@$dns: &>log/log-script-tx-$dns
+
+  [ $? -ne 0 ] && echo "*** ERROR $? RETURNED FOR ${dns} WHILST COPYING SCRIPTS, bailing ***" && exit 1
+
   bin/issh -i ${identity} -tt cloud-user@$dns sudo 'bash -c "cd /home/cloud-user; ./openshift-aws-reip.sh"' &>log/log-ip-$dns < /dev/null &
+
+  [ $? -ne 0 ] && echo "*** ERROR $? RETURNED FOR ${dns} WHILST RUNNING REIP SCRIPT, bailing ***" && exit 1
+
 done < creds.csv
 
 echo "*** WAITING FOR IPs TO BE UPDATED ***"
@@ -48,7 +56,12 @@ for dns in "${hosts[@]}"
 do
 
   bin/issh -i ${identity} -tt cloud-user@$dns 'bash -c "ps -ax|grep xvda"' &>>log/log-xvda-$dns < /dev/null
+
+  [ $? -ne 0 ] && echo "*** ERROR $? RETURNED FOR ${dns} WHILST LOOKING FOR xvda ***" 
+
   bin/issh -i ${identity} -tt cloud-user@$dns sudo ./warm-openshift.sh &>log/log-warmup-$dns < /dev/null &
+  
+  [ $? -ne 0 ] && echo "*** ERROR $? RETURNED FOR ${dns} WHILST WARMING UP OPENSHIFT ***" 
 
 done
 
--- provision-novnc.sh
+++ provision-novnc.sh
@@ -11,11 +11,7 @@ do
   IFS=, vals=($line)
   IFS=$OLDIFS
 
-  name=${vals[0]}
-  ip=${vals[1]}
   dns=$(echo ${vals[2]} | cut -d '"' -f2)
-  pwd=$(echo ${vals[3]} | cut -d '"' -f2)
-
 
   scp -i ${identity} -S bin/issh novnc/* cloud-user@$dns: &>log/log-vnc-$dns
   bin/issh -i ${identity} -tt cloud-user@$dns sudo 'bash -c "cd /home/cloud-user; ./create-novnc.sh"' &>log/log-vnc-$dns < /dev/null &
