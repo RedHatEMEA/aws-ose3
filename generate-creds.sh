5f700d3d502a4bcb1788710b2edb76e31411a2c1
Jim Minter <jminter@redhat.com>
2015-10-30 16:08:23 +0000

    style and move print-instances.py into bin

--- generate-creds.sh
+++ generate-creds.sh
@@ -5,7 +5,7 @@ filter=$1
 
 [ -e creds.csv ] && rm -f creds.old && mv creds.{csv,old} && rm -f creds.csv
 
-aws ec2 describe-instances --filters "Name=tag:RunName,Values=${filter}" | python print-instances.py > creds.csv
+aws ec2 describe-instances --filters "Name=tag:RunName,Values=${filter}" | python bin/print-instances.py > creds.csv
 
 
 
