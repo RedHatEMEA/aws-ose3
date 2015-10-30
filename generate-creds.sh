56cba323d976ba37fe340554b99a682cdb5c301b
Jim Minter <jminter@redhat.com>
2015-10-30 16:25:48 +0000

    shell script cleanup

--- generate-creds.sh
+++ generate-creds.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-filter=$1
-[ "${filter}" == "" ] && echo "** Please provide RunName tag value **" && exit 1
+RUNNAME=$1
+[ -z "$RUNNAME" ] && echo "** Please provide RunName tag value **" && exit 1
 
-[ -e creds.csv ] && rm -f creds.old && mv creds.{csv,old} && rm -f creds.csv
-
-aws ec2 describe-instances --filters "Name=tag:RunName,Values=${filter}" | python bin/print-instances.py > creds.csv
-
-
-
+aws ec2 describe-instances --filters "Name=tag:RunName,Values=$RUNNAME" | python bin/print-instances.py > creds.csv
--- start.sh
+++ start.sh
@@ -9,39 +9,26 @@ SECGROUP=sg-a4bc1ac0
 SUBNET=subnet-7d690824
 
 # generate an ignore list
-ignore=$(aws ec2 describe-instances | grep InstanceId | awk '{print $2}' | cut -d '"' -f2)
+ignore=$(aws ec2 describe-instances | awk '/InstanceId/ {print $2}' | tr -d '",')
 
- aws ec2 run-instances --image-id $AMI --key-name $KEYNAME \
-   --associate-public-ip-address \
-   --security-group-ids $SECGROUP --instance-type m4.large \
-   --subnet-id $SUBNET --ebs-optimized \
-   --block-device-mappings '{"DeviceName":"/dev/sda1","Ebs":{"DeleteOnTermination":"true","VolumeType":"gp2"}}' \
-   --count $N
+aws ec2 run-instances --image-id $AMI --key-name $KEYNAME \
+  --associate-public-ip-address \
+  --security-group-ids $SECGROUP --instance-type m4.large \
+  --subnet-id $SUBNET --ebs-optimized \
+  --block-device-mappings '{"DeviceName": "/dev/sda1", "Ebs": {"DeleteOnTermination": true, "VolumeType": "gp2"}}' \
+  --count $N
 
-# 
-
-# remove any existing list of instances
-[ -e instances.list ] && rm -f instances.oldlist && mv instances.{list,oldlist}
-rm -f instances.list
-
-id=1
-tag=$(date +"%Y%d%m-%H%M")
+RUNNAME=$(date +%Y%m%d%H%M%S)
 
 echo "** Use RunName to select this batch of instances for future operations **"
-echo "RunName=${tag}"
+echo "RunName=$RUNNAME"
 
 # dump out a new list of instances creating using the above start command
-aws ec2 describe-instances | grep InstanceId | awk '{print $2}' | cut -d '"' -f2 | while read instanceID
+id=1
+aws ec2 describe-instances | awk '/InstanceId/ {print $2}' | tr -d '",' | while read instanceID
 do
-
    if [[ ${ignore} != *"${instanceID}"* ]]; then
-
-      echo "${instanceID}" >> instances.list 
-      aws ec2 create-tags --resources ${instanceID} --tags Key=Name,Value="inst-${tag}-${id}" Key=RunName,Value="${tag}"
+      aws ec2 create-tags --resources "$instanceID" --tags Key=Name,Value="ose3-$RUNNAME-$id" Key=RunName,Value="$RUNNAME"
       id=$((id+1))
-
-   fi 
-
+   fi
 done
-
-
