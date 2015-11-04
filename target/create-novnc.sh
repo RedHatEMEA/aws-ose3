a919c34e7129655c89f4c61c740d7be84f630536
Jim Minter <jminter@redhat.com>
2015-11-04 12:20:30 +0000

    fix novnc glitch

--- target/create-novnc.sh
+++ target/create-novnc.sh
@@ -3,7 +3,7 @@
 docker pull docker.io/fedora:22
 oadm new-project vnc
 oc project vnc
-oc process -f novnc.yaml | oc create -f -
+oc create -f novnc.yaml
 oc env dc novnc HOSTPORT=$(hostname):5900
 sleep 30
 oc get is fedora
