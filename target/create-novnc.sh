0a9f88eb1ea24555f5428ad4c42e7f3979fc3a70
Jim Minter <jminter@redhat.com>
2016-02-25 11:24:50 +0000

    simplifications, hopefully not over-simplifications

--- target/create-novnc.sh
+++ target/create-novnc.sh
@@ -1,31 +1,12 @@
 #!/bin/bash -x
 
-oc delete project vnc
-while [ $(oc get project vnc --no-headers | wc -l) -gt 0 ]; do
-   echo "Waiting for project to be deleted"
-   oc get project vnc
-   sleep 1
-done
-
 oadm new-project vnc
-oc project vnc
-
-while [  $(oc get routes no | wc -l) -gt 0 ]; do
-   echo "old route still with us, waiting for it to disappear"
-   oc delete route no
-   oc get route no
-   sleep 2
-done
-
+oc delete all --all -n vnc
 oc new-app -n vnc --docker-image=jimminter/novnc -e HOSTPORT=$(hostname):5900
-
-echo "creating new route"
-oc create -f - <<API
+oc create -n vnc -f - <<EOF
 apiVersion: v1
 kind: Route
 metadata:
-  labels:
-    app: novnc
   name: "no"
 spec:
   tls:
@@ -33,5 +14,4 @@ spec:
   to:
     kind: Service
     name: novnc
-API
-
+EOF
