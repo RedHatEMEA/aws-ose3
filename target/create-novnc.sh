5e64e9803a1bbacca80f7af5894530263fe6b3cd
Ed Seymour <eseymour@redhat.com>
2016-02-25 10:06:47 +0000

    The create-novnc script did not execute correctly if run more than once, have added a test to ensure that we're starting from a clean slate. The exposed route wasn't SSL (just plain HTTP), updated to create a TLS edge terminated route.

--- target/create-novnc.sh
+++ target/create-novnc.sh
@@ -1,5 +1,37 @@
 #!/bin/bash -x
 
+oc delete project vnc
+while [ $(oc get project vnc --no-headers | wc -l) -gt 0 ]; do
+   echo "Waiting for project to be deleted"
+   oc get project vnc
+   sleep 1
+done
+
 oadm new-project vnc
+oc project vnc
+
+while [  $(oc get routes no | wc -l) -gt 0 ]; do
+   echo "old route still with us, waiting for it to disappear"
+   oc delete route no
+   oc get route no
+   sleep 2
+done
+
 oc new-app -n vnc --docker-image=jimminter/novnc -e HOSTPORT=$(hostname):5900
-oc expose -n vnc svc/novnc --name no
+
+echo "creating new route"
+oc create -f - <<API
+apiVersion: v1
+kind: Route
+metadata:
+  labels:
+    app: novnc
+  name: "no"
+spec:
+  tls:
+    termination: edge
+  to:
+    kind: Service
+    name: novnc
+API
+
